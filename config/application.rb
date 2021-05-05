fcea2e5b0c17f360c3e51d13ebfe3473377f26d8
Grant Kayes <glk25@georgetown.edu>
2021-05-05 15:30:52 -0700

    make it better

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module SetworkApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000', 'https://lit-inlet-82685.herokuapp.com/' 
+        origins '*'
         resource '*', :headers => :any, :methods => [:get]
       end
     end
